3c05fde161710d10146f327faadd68d59eeb8d35
Guilherme Rodrigues <gadr90@gmail.com>
2015-07-03 18:47:20 -0300

    Fix double quotes

--- src/utils/getCookie.js
+++ src/utils/getCookie.js
@@ -1,5 +1,7 @@
 export default function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
+  var value = '; ' + document.cookie;
+  var parts = value.split('; ' + name + '=');
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
 }
