d415321ae4f2a5d73f98343d817d469d9c6d5940
Breno Calazans <breno@vtex.com.br>
2015-07-07 16:10:33 -0300

    Improve Navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router';
-import Style from './header.less';
+import { Navigation } from 'react-router';
+import Style from './header.less'; // eslint-disable-line
 import {
-  Navbar, Nav,
-  DropdownButton,
-  MenuItem,
-  Grid, Row, Col
+  Navbar, Nav, NavItem
 } from 'react-bootstrap';
 
 let Header = React.createClass({
+  mixins: [ Navigation ],
   render() {
     return (
-      <div>
-        <Navbar brand='Comuna'>
-          <Nav>
-            <DropdownButton title='Menu'>
-              <Link to="home">
-                <MenuItem>Home</MenuItem>
-              </Link>
-            </DropdownButton>
-          </Nav>
-        </Navbar>
-      </div>
+      <Navbar brand='Comuna' inverse toggleNavKey={0}>
+        <Nav right eventKey={0}> {/* This is the eventKey referenced */}
+          <NavItem eventKey='1' onClick={() => this.transitionTo('home')}>Home</NavItem>
+        </Nav>
+      </Navbar>
     );
   }
 });
